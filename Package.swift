796105d1eb8b9be2b962ac640ed26c29384938e6
Usman Nazir <usmannzr@live.com>
2025-11-09 00:29:09 +0000

    Update name + basic structure

--- Package.swift
+++ Package.swift
@@ -4,19 +4,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "puzzlens-ios",
+    name: "PuzzlensSDK",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "puzzlens-ios",
-            targets: ["puzzlens-ios"]
+            name: "PuzzlensSDK",
+            targets: ["PuzzlensSDK"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "puzzlens-ios"
+            name: "PuzzlensSDK"
         ),
 
     ]
